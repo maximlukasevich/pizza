57c96fa868c3d802bd6cce30da5930e51dfbef5a
maximlukasevich <maxim.lukasevich@gmail.com>
2021-06-17 22:31:06 +0300

    Created CheckBox component

---
+++ web/src/components/common/CheckBox/CheckBox.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import nextId from 'react-id-generator';
+import styles from './checkbox.module.css';
+import {CheckOutlined} from '@ant-design/icons';
+
+const CheckBox = (props) => {
+
+  const componentId = nextId();
+
+  return (
+    <div className={styles.checkbox}>
+
+      <input
+        className={styles.input}
+        type="checkbox"
+        value={props.value}
+        id={componentId}
+      />
+      <label
+        className={styles.label}
+        htmlFor={componentId} >
+        {props.label}
+
+        <span className={styles.check}>
+          <CheckOutlined className={styles.icon} />
+        </span>
+
+      </label>
+
+    </div>
+  );
+};
+
+export default CheckBox;
