30ff76a22754a29fd6cf3130b6f1f87fc5458091
maximlukasevich <maxim.lukasevich@gmail.com>
2021-06-17 17:41:42 +0300

    Created Input common component

---
+++ web/src/components/common/Input/Input.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './input.module.css';
+
+const Input = (props) => {
+  return (
+    <div className={styles.inputDiv}>
+      <label className={styles.label}>
+        {props.label}:
+      </label>
+      <input
+        className={styles.input}
+        type={props.type}
+        value={props.value}
+        placeholder={props.placeholder} />
+    </div>
+
+  );
+};
+
+export default Input;
