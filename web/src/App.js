2207702d7c097cdd291106dc18d01269c9ad3422
maximlukasevich <maxim.lukasevich@gmail.com>
2021-06-17 22:32:13 +0300

    Added new components

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,33 @@
-
+import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import Pizza from './pages/Pizza/Pizza';
+import PizzaDetail from './pages/PizzaDetail/PizzaDetail';
+import Cart from "./pages/Cart/Cart";
+import Auth from "./pages/Admin/Auth/Auth";
+import PizzaList from "./pages/Admin/PizzaList/PizzaList";
+import CreatePizza from "./pages/Admin/CreatePizza/CreatePizza";
+import UpdatePizza from "./pages/Admin/UpdatePizza/UpdatePizza";
+import Orders from "./pages/Admin/Orders/Orders";
 
 function App() {
   return (
-    <div>
-      <h1>gay</h1>
+    <div className='container'>
+      <Navigation />
+      <div className='content'>
+        <div className='contentBody'>
+
+          <Orders />
+          <UpdatePizza />
+          <CreatePizza />
+          <PizzaList />
+          <Auth />
+
+          <Cart />
+          <PizzaDetail />
+          <Pizza />
+
+        </div>
+      </div>
     </div>
   );
 }
