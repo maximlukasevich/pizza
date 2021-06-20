d40ffe27c6dc0b8478aa4dd22a073b2a2ec17e00
maximlukasevich <maxim.lukasevich@gmail.com>
2021-06-20 17:18:43 +0300

    Added functionality

--- web/src/components/PopularPizza/PopularPizza.jsx
+++ web/src/components/PopularPizza/PopularPizza.jsx
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import styles from './popular-pizza.module.css';
 import PizzaCard from '../../pages/Pizza/components/PizzaCard/PizzaCard';
+import {useDispatch, useSelector} from 'react-redux';
+import {getAllPizza} from '../../store/Pizza/actions';
 
 const PopularPizza = (props) => {
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getAllPizza());
+  }, [dispatch]);
+
+  const pizza = useSelector(state => state.pizza.allPizza
+    .filter((item) => item.inStock)
+    .sort((a, b) => -1 * (a.orderedTimes - b.orderedTimes))
+    .slice(0, 4)
+  );
+
+  const pizzaCartRender = pizza.map((item, i) =>
+    item.inStock ?
+      <PizzaCard
+        image={item.image}
+        name={item.name}
+        slug={item.slug}
+        sizes={item.sizes}
+        key={i}
+      /> : '');
+
+
   return (
     <div className={props.className}>
       <h1 className={styles.title}>Найчастіше замовляють:</h1>
       <div className={styles.pizza}>
-        <PizzaCard name={'Маргарита'}/>
-        <PizzaCard name={'Капрічоза'}/>
-        <PizzaCard name={'Вогняний Дракон'}/>
-        <PizzaCard name={'Класична'}/>
+        {pizzaCartRender}
       </div>
     </div>
   );
