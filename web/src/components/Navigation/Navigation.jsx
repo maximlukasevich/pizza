7b6ca5ccdbb5856d184cbcb5bdd0d778108d74da
maximlukasevich <maxim.lukasevich@gmail.com>
2021-06-22 19:51:48 +0300

    Code fix

--- web/src/components/Navigation/Navigation.jsx
+++ web/src/components/Navigation/Navigation.jsx
@@ -1,22 +1,19 @@
 import React from 'react';
+import {NavLink} from "react-router-dom";
 import styles from './navigation.module.css';
 import CartButton from '../CartButton/CartButton';
 import Logo from "../Logo/Logo";
-import {NavLink} from "react-router-dom";
-import AdminNavigation from "../AdminNavigation/AdminNavigation";
+import HR from "../common/HR/HR";
 
-const Navigation = (props) => {
+const Navigation = () => {
 
   return (
     <div>
       <nav className={styles.nav}>
-
         <NavLink to={'/pizza'}> <Logo /> </NavLink>
         <NavLink to={'/cart'}> <CartButton /> </NavLink>
-
       </nav>
-      {props.isAuth && <AdminNavigation />}
-      <hr className={styles.hr}/>
+      <HR className={styles.hr} />
     </div>
   );
 };
